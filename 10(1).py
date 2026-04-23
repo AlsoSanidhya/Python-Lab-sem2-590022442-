d1e41b10fb3929f65f09c06cc45e0290a95e246e
AlsoSanidhya <waynesfr@gmail.com>
2026-04-23 22:12:34 +0530

    Update 10(1).py

--- 10(1).py
+++ 10(1).py
@@ -2,7 +2,7 @@
 import numpy as np
 
 arr = np.array([10, 20, 30, 40, 50])
-total_sum = np.sum(arr)
+tsum = np.sum(arr)
 
 print("Array:", arr)
-print("Sum of all elements:", total_sum)
+print("Sum of all elements:", tsum)
